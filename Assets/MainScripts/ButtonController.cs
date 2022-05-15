2cd263fb1283e6f633b2a3b8285c90108436eed2
IlyaKochura <102990933+IlyaKochura@users.noreply.github.com>
2022-05-15 16:30:37 +0300

    1

---
+++ Assets/MainScripts/ButtonController.cs
@@ -0,0 +1,27 @@
+using System;
+using MechanicsGameS;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class ButtonController : MonoBehaviour
+{
+    public MechanicsGame  _mechanicsGame;
+    public Button _button;
+
+
+    private delegate void ButtonClicked(string x, string o);
+
+    public Action Test { get; set;}
+    
+    public void Start()
+    {
+        _button.onClick.AddListener( () => Test.Invoke());
+    }
+
+    public void ChangeText(string newText)
+    {
+        GetComponentInChildren<Text>().text = newText;
+    }
+}
---
+++ Assets/MainScripts/MechanicsGame.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace MechanicsGameS
+{
+    public class MechanicsGame : MonoBehaviour
+    {
+        [SerializeField] private List<ButtonController> _battleField;
+        public static int _counter = 1;
+        
+        
+
+        private void Start()
+        {
+            InvokeRepeating("CheckWin", 1, 1);
+            
+            for (var i = 0; i < _battleField.Count; i++)
+            {
+                int id = i;
+                _battleField[i].Test = () => ButtonMove(id);
+            }
+        }
+
+        private void Restart()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        private void ButtonMove(int index)
+        {
+            
+            var but = _battleField[index];
+            if (_counter % 2 == 0)
+            {
+                but.ChangeText("O");
+            }
+            else
+            {
+                but.ChangeText("X");
+            }
+            
+            _counter++;
+        }
+    }
+}
